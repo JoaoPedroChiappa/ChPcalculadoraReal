a44ff1303e4bc7505b0b8cc60a150508bf514589
JoaoPedroChiappa <joaopedrochiappa@edu.uniube.br>
2022-03-13 22:21:43 -0300

    +. -. *, /

--- AndroidChP/app/src/main/java/com/example/chpcalculadora/MainActivity.java
+++ AndroidChP/app/src/main/java/com/example/chpcalculadora/MainActivity.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    //txtnum1; num1; txtnum2; num2; btnMais; btnMenos; btnMulti; btnDivisao; txtresposta; cmpresposta; btnMemoria1; btnMemoria2; btnMemoria3; btnMemoria4; btnFinalizar;
+    //num1; num2; btnMais; btnMenos; btnMulti; btnDivisao; cmpresposta; btnMemoria1; btnMemoria2; btnMemoria3; btnMemoria4; btnMemoria5; btnFinalizar;
+
+    ImageButton btnMais, btnMenos, btnMulti, btnDivisao, btnMemoria1, btnMemoria2, btnMemoria3, btnMemoria4, btnMemoria5;
+
+    TextView num1, num2, cmpresposta;
+    double resposta = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,83 @@ public class MainActivity extends AppCompatActivity {
                 System.exit(0);
             }
         });
+
+        num1 = findViewById(R.id.num1);
+        num2 = findViewById(R.id.num2);
+
+        btnMais = findViewById(R.id.btnMais);
+        btnMenos = findViewById(R.id.btnMenos);
+        btnMulti = findViewById(R.id.btnMulti);
+        btnDivisao = findViewById(R.id.btnDivisao);
+
+        btnMemoria1 = findViewById(R.id.btnMemoria1);
+        btnMemoria2 = findViewById(R.id.btnMemoria2);
+        btnMemoria3 = findViewById(R.id.btnMemoria3);
+        btnMemoria4 = findViewById(R.id.btnMemoria4);
+        btnMemoria5 = findViewById(R.id.btnMemoria5);
+
+        cmpresposta = findViewById(R.id.cmpresposta);
+
+        btnMais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+                resposta = a + b;
+                String respostaFinal = String.valueOf(resposta);
+                cmpresposta.setText(respostaFinal);
+                resposta = 0;
+            }
+        });
+
+        btnMenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+                resposta = a - b;
+                String respostaFinal = String.valueOf(resposta);
+                cmpresposta.setText(respostaFinal);
+                resposta = 0;
+            }
+        });
+
+        btnMulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+                resposta = a * b;
+                String respostaFinal = String.valueOf(resposta);
+                cmpresposta.setText(respostaFinal);
+                resposta = 0;
+            }
+        });
+
+        btnDivisao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+                resposta = a / b;
+                String respostaFinal = String.valueOf(resposta);
+                cmpresposta.setText(respostaFinal);
+                resposta = 0;
+            }
+        });
+
+        /*
+        btnMemoria1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(num1.getText().toString());
+                double b = Double.parseDouble(num2.getText().toString());
+                resposta = a / b;
+                String respostaFinal = String.valueOf(resposta);
+                cmpresposta.setText(respostaFinal);
+                resposta = 0;
+            }
+        });
+        */
     }
 }
