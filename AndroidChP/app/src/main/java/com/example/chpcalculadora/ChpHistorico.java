2700b45ef26fc70aaa6ce41bd1c9a51abc8d6c03
JoaoPedroChiappa <joaopedrochiappa@edu.uniube.br>
2022-03-20 16:14:26 -0300

    Confirmação de apagar hisórico

--- AndroidChP/app/src/main/java/com/example/chpcalculadora/ChpHistorico.java
+++ AndroidChP/app/src/main/java/com/example/chpcalculadora/ChpHistorico.java
@@ -1,9 +1,11 @@
 package com.example.chpcalculadora;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -22,7 +24,7 @@ import java.io.IOException;
 public class ChpHistorico extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chp_historico);
 
@@ -47,18 +49,8 @@ public class ChpHistorico extends AppCompatActivity {
 
         btnApagaHistorico.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                try {
-                    FileOutputStream arquivoGravar = openFileOutput("calc.txt", MODE_PRIVATE);
-                    String msg = "";
-                    arquivoGravar.write(msg.getBytes());
-                    arquivoGravar.close();
-                    //Ao invés de chamar o arquivo de novo, passo msg em branco, sabendo que o arquivo foi apagado nas linhas acima.
-                    txtHistorico.setText("");
-                }
-                catch (IOException error){
-
-                }
+            public void onClick(View v) {
+                confirmacao();
             }
         });
 
@@ -75,7 +67,43 @@ public class ChpHistorico extends AppCompatActivity {
                 System.exit(0);
             }
         });
+    }
 
+    private AlertDialog alerta;
+
+    private void confirmacao(){
+
+        TextView txtHistorico = findViewById(R.id.txtHistorico);
+
+        AlertDialog.Builder msgBox = new AlertDialog.Builder(this);
+        msgBox.setTitle("Excluindo...");
+        msgBox.setIcon(android.R.drawable.ic_menu_delete);
+        msgBox.setMessage("Tem certeza que deseja excluir o histórico?");
+        msgBox.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                try {
+                    FileOutputStream arquivoGravar = openFileOutput("calc.txt", MODE_PRIVATE);
+                    String msg = "";
+                    arquivoGravar.write(msg.getBytes());
+                    arquivoGravar.close();
+                    //Ao invés de chamar o arquivo de novo, passo msg em branco, sabendo que o arquivo foi apagado nas linhas acima.
+                    txtHistorico.setText("");
+                }
+                catch (IOException error){
+
+                }
+                Toast.makeText(ChpHistorico.this, "Histórico apagado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        msgBox.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(ChpHistorico.this, "Histórico não foi apagado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alerta = msgBox.create();
+        alerta.show();
     }
 
 }
